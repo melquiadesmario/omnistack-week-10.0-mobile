aba610c2c7892a6237a6a3d23562093d7a316f0f
melquiadesmario <melquiadesm@gmail.com>
2020-05-26 21:44:39 -0300

    Socket IO

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -5,6 +5,7 @@ import { requestPermissionsAsync, getCurrentPositionAsync} from 'expo-location'
 import { MaterialIcons } from '@expo/vector-icons'
 
 import api from '../services/api'
+import { connect, disconnect, subscribeToNewDevs } from '../services/socket'
 
 const Main = ({ navigation }) => {
     const [devs, setDevs] = useState([])
@@ -33,6 +34,22 @@ const Main = ({ navigation }) => {
         loadInitialPosition()
     }, [])
 
+    useEffect(() => {
+        subscribeToNewDevs(dev => setDevs([...devs, dev]))
+    }, [devs])
+
+    function setupWebsocket(){
+        disconnect()
+
+        const { latitude, longitude } = currentRegion
+        
+        connect(
+            latitude,
+            longitude,
+            techs
+        )
+    }
+
     async function loadDevs(){
         const { latitude, longitude } = currentRegion
 
@@ -44,6 +61,7 @@ const Main = ({ navigation }) => {
             }
         })
         setDevs(response.data.devs)
+        setupWebsocket()
     }
 
     function handleRegionChanged(region){
